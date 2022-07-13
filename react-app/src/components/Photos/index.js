458d47bee7ea8a06f010817ab549db7884222869
chris <Chrispyohong@gmail.com>
2022-07-13 06:57:41 -0700

    refactored PhotosPage code to make it more modular

--- react-app/src/components/Photos/index.js
+++ react-app/src/components/Photos/index.js
@@ -7,6 +7,7 @@ import CommentsPage from '../Comments'
 import CommentFormPage from '../CommentForm';
 import PhotoLikesForm from '../PhotoLikesForm'
 import bulletPoints from './bulletPoints.png'
+import PhotoDisplay from '../PhotoDisplay';
 import './Photos.css';
 
 
@@ -46,8 +47,8 @@ function PhotosPage() {
                 return (
                     <div className='photo-container'>
                         <div className='FullImageContainer'>
-
-                            <div className='post-title-edit-deleteform'>
+                            <PhotoDisplay photo={photo}/>
+                            {/* <div className='post-title-edit-deleteform'>
                                 <h2>{photo.username.username}</h2>
                                 {photo.userId === userId ?
                                     <div className='Edit-Delete-Photo-Container'>
@@ -102,7 +103,7 @@ function PhotosPage() {
                             {<CommentsPage photo={photo.id} />}
                         </div>
                         <div className='comments-form-div'>
-                            {<CommentFormPage photoId={photo.id} />}
+                            {<CommentFormPage photoId={photo.id} />} */}
                         </div>
                         <p>{photo.createdAt}</p>
                     </div>
