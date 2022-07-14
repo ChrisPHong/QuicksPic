97f2d6d508205b64bed5242bb77d0cec13546667
chris <Chrispyohong@gmail.com>
2022-07-13 20:26:58 -0700

    added the nav bar css

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -18,73 +18,82 @@ const NavBar = () => {
 
   const showPhotoForm = () => {
     if (show === false) {
-        return setShow(true)
+      return setShow(true)
     }
     if (show === true) {
-        return setShow(false)
+      return setShow(false)
     }
-}
+  }
 
   return (
     <div className='navigationbar-Div'>
       {currentUser ?
         <>
-          <div className='home'>
+          <div className='signedIn-user-nav-container'>
 
-            <NavLink to='/' exact={true} className='home' activeClassName='active'>
-          <img className='quickPicTitle-picture' alt='logo-quicksPic' src={quickpicTitle} />
-            </NavLink>
+              <div className='home'>
+
+                <NavLink to='/' exact={true} className='home' activeClassName='active'>
+                  <img className='quickPicTitle-picture' alt='logo-quicksPic' src={quickpicTitle} />
+                </NavLink>
+              </div>
+
+            <div className='home-post-logout-container'>
+              <div className='home'>
+
+                <NavLink to='/' exact={true} className='home' activeClassName='active'>
+                  <img className='quickPicLogo-picture' alt='logo-quicksPic' src={home} />
+                </NavLink>
+              </div>
+
+              <div>
+                <button onClick={showPhotoForm}
+                  className='PhotoFormButton'>
+                  <img className='photo-Form-logo' alt='logo-quicksPic' src={createPost} />
+                </button>
+                {show ?
+                  <PhotoForm />
+                  : null}
+              </div>
+
+
+              {/* <div className='users'>
+
+<NavLink to='/users' exact={true} className='users' activeClassName='active'>
+Users
+</NavLink>
+</div> */}
+
+              <div className='logout'>
+                <LogoutButton />
+              </div>
+
+            </div>
           </div>
-
-          <div className='home'>
-
-            <NavLink to='/' exact={true} className='home' activeClassName='active'>
-          <img className='quickPicLogo-picture' alt='logo-quicksPic' src={home} />
-            </NavLink>
-          </div>
-
-        <div>
-        <button onClick={showPhotoForm}
-        className='PhotoFormButton'>
-          <img className='photo-Form-logo' alt='logo-quicksPic' src={createPost} />
-        </button>
-        {show ?
-          <PhotoForm />
-        : null}
-        </div>
-
-
-          {/* <div className='users'>
-
-            <NavLink to='/users' exact={true} className='users' activeClassName='active'>
-              Users
-            </NavLink>
-          </div> */}
-
-          <div className='logout'>
-            <LogoutButton />
-          </div>
-
         </>
         :
         <>
-          <div className='home'>
-            <NavLink to='/' exact={true} className='home' activeClassName='active'>
-            <img className='quickPicLogo-picture' alt='logo-quicksPic' src={quickpicLogo} />
-            </NavLink>
-          </div>
-          <div className='sign-up'>
+          <div className='entire-navBar-Container'>
 
-            <div className='login'>
-
-              <NavLink to='/login' exact={true} className='login' activeClassName='active'>
-                Login
+            <div className='home'>
+              <NavLink to='/' exact={true} className='home' activeClassName='active'>
+                <img className='quickPicTitle-picture' alt='logo-quicksPic' src={quickpicTitle} />
               </NavLink>
             </div>
-            <NavLink to='/sign-up' exact={true} className='sign-up' activeClassName='active'>
-              Sign Up
-            </NavLink>
-          </div >
+            <div className='login-signup-container'>
+
+              <div className='login'>
+                <NavLink to='/login' exact={true} className='login-navlink' activeClassName='active'>
+                  Login
+                </NavLink>
+              </div>
+              <div className='sign-up'>
+                <NavLink to='/sign-up' exact={true} className='sign-up-' activeClassName='active'>
+                  Sign Up
+                </NavLink>
+              </div >
+            </div>
+          </div>
         </>
 
 
