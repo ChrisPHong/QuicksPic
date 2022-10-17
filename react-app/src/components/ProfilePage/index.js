cac56a436c5273378e1930655edc253b62bcdc84
chris <Chrispyohong@gmail.com>
2022-10-17 14:04:49 -0700

    adding profile components together

---
+++ react-app/src/components/ProfilePage/index.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import ProfileTopPortion from '../ProfileTopForm';
+import UserProfilePage from '../UserProfilePage';
+
+function ProfilePage (){
+
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(() =>{
+        window.scrollTo(0,0)
+        setIsLoading(true);
+        setTimeout(()=>{
+            setIsLoading(false)
+        }, 1000)
+    }, [])
+
+    return (
+        <>
+        {isLoading ?
+
+            <div className='loading-ctn'>
+                <div className='loading'>testing</div>
+
+            </div>
+            :
+            <>
+            <ProfileTopPortion />
+            <UserProfilePage />
+            </>
+            }
+            </>
+    )
+}
+
+export default ProfilePage;
